701b58dbd3a092f8f493ecdce35f9175ade3db7e
Ricardo AM4 <ricardo.chaves.am4@outlook.com>
2017-09-28 16:04:46 -0300

    iniciando usuario

--- models/Users.js
+++ models/Users.js
@@ -1,45 +1,45 @@
 import bcrypt from 'bcrypt';
 
 export default (sequelize, DataType) => {
-    const Users = sequelize.define('Users', {
-        id: {
-            type: DataType.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataType.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        email: {
-            type: DataType.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        },
-        password: {
-            type: DataType.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        }
+  const Users = sequelize.define('Users', {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
+    name: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+
+  },
     {
-        hooks:{
-            beforeCreate: user => {
-                const salt = bcrypt.gemSaltSync();
-                user.set('password', bcrypt.hashSync(user.password, salt));
-            }
+      hooks: {
+        beforeCreate: user => {
+          const salt = bcrypt.genSaltSync();
+          user.set('password', bcrypt.hashSync(user.password, salt));
         },
-        classMethods:{
-            isPassword: (encodePassword, password) => bcrypt.compareSync(password, encodePassword);
-        }
+      },
+      classMethods: {
+        isPassword: (encodedPassword, password) => bcrypt.compareSync(password, encodedPassword),
+      },
     });
-    return Users;
-}
-    
+  return Users;
+};
